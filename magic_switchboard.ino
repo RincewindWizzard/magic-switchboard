a8adfc0cf54b90c0484551f02d0a45a0e7fda10d
RincewindWizzard <RincewindWizzard@0xdf.de>
2016-04-29 20:58:47 +0200

    initial

---
+++ magic_switchboard.ino
@@ -0,0 +1,28 @@
+/*
+ * wiring.h
+
+      Attiny 2313
+            ______
+Reset      |      | Vdd
+           |      | PB7 Lamp 1
+           |      | PB6 Lamp 2
+           |      | PB5 Lamp 3
+           |      | PB4 Lamp 4
+           |      | PB3 Switch 1
+           |      | PB2 Switch 2
+           |      | PB1 Switch 3
+PD5 Alarm  |      | PB0 Switch 4
+GND        |______| PD6 Clear Button
+
+ *  Created on: 10.04.2014
+ *      Author: fxw
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

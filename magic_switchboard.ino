58794945c31e723b48fc49bfc6c663e4a7e4fe29
Rincewind <git@magierdinge.de>
2017-06-24 16:45:19 +0200

    works now for atmega328p mcu

--- magic_switchboard.ino
+++ magic_switchboard.ino
@@ -11,63 +11,51 @@ When the audience returns his box he remembers the sequence in which the lights 
 
 For the audience it seems like the colors are connected somehow.
 
-This version is designed for the Attiny 2313 using the following pin Layout.
-
-      Attiny 2313
-            ______
-Reset      |      | Vdd
-           |      | PB7 Lamp 1
-           |      | PB6 Lamp 2
-           |      | PB5 Lamp 3
-           |      | PB4 Lamp 4
-           |      | PB3 Switch 4
-           |      | PB2 Switch 3
-           |      | PB1 Switch 2
-           |      | PB0 Switch 1
-GND        |______| 
-
  */
-
-// this is the mask to get our input
-#define SCHWITCH_MASK 0b1111
-
-// this is the mask for our output
-#define LAMP_MASK 0b11110000
-
-// how long do we wait when turned off, to reset and powerdown
-#define RESET_DELAY 2500
 
 // how many lamps/switches are connected
 #define LAMP_COUNT 4
 
-// shift a mask up the output mask
-#define LAMP_SHIFT 4
+typedef uint8_t lamp_t;
+typedef uint8_t switch_t;
+
+const lamp_t LAMP1 = 6;
+const lamp_t LAMP2 = 7;
+const lamp_t LAMP3 = 8;
+const lamp_t LAMP4 = 9 ;
+
+const switch_t SWITCH1 = 2;
+const switch_t SWITCH2 = 3;
+const switch_t SWITCH3 = 4;
+const switch_t SWITCH4 = 5;
+
+#define ON 1
+#define OFF 0
+
+// how long do we wait when turned off, to reset and powerdown
+#define RESET_DELAY 50000
+
+
 
 // this switch is not connected, yet
 #define DISCONNECTED (0xFF)
 
-// power modes
-// in idle the main loop is paused, but the timer and the pcint are running
-#define idle( )   MCUCR = MCUCR & 0b10001111; MCUCR |= 1 << SE | 0 << SM1 | 0 << SM0;
-
-// in powerdown the timers and main loop are stopped, can only be woken up by PCINT (turning a switch)
-#define powerdown( ) MCUCR = MCUCR & 0b10001111; MCUCR |= 1 << SE | 1 << SM1 | 1 << SM0;
 
 // which switch is connected to which lamp
 // connections[switch] = lamp
-byte connections[LAMP_COUNT] = {DISCONNECTED, DISCONNECTED, DISCONNECTED, DISCONNECTED };
+uint8_t connections[LAMP_COUNT] = {DISCONNECTED, DISCONNECTED, DISCONNECTED, DISCONNECTED };
 
 // which was the last swich changed?
-byte last_changed = 0;
+uint8_t last_changed = 0;
 
 // current sequence used for assigning new lamps to unconnected switches
-byte sequence = 0;
+uint8_t sequence = 0;
 
 // how many lamps have already be connected
-byte current_lamp = 0;
+uint8_t current_lamp = 0;
 
 // sequences for new lamp connections
-byte lamp_ordering[LAMP_COUNT][LAMP_COUNT] = {
+uint8_t lamp_ordering[LAMP_COUNT][LAMP_COUNT] = {
   { 0, 1, 2, 3 },
   { 1, 0, 2, 3 },
   { 2, 3, 1, 0 },
@@ -75,39 +63,97 @@ byte lamp_ordering[LAMP_COUNT][LAMP_COUNT] = {
 };
 
 // this mask stores the previous state of input
-byte last_switches = 0b0000;
+uint8_t last_switches = 0b0000;
 
 // this mask stores the current state of input
-byte current_switches = 0b0000;
- 
+uint8_t current_switches = 0b0000;
+
 void setup() {
-  // sets the LEDs to OUTPUT and the switches to input
-  DDRB = 0b11110000;
+  pinMode(LAMP1, OUTPUT);
+  pinMode(LAMP2, OUTPUT);
+  pinMode(LAMP3, OUTPUT);
+  pinMode(LAMP4, OUTPUT);
 
+  pinMode(SWITCH1, INPUT);
+  pinMode(SWITCH2, INPUT);
+  pinMode(SWITCH3, INPUT);
+  pinMode(SWITCH4, INPUT);
+
+  cli();
   // activate pin change interrupt for switches
-  PCMSK |= 0b00001111;
-  GIMSK |= 1 << PCIE;
-
-  // set sleep modes
-  idle();
-
-  // update switches
-  PORTB = LAMP_MASK & (PINB << 4);
-
-  // set up timer 1
-  TCCR1B = (TCCR1B & ~0b111) | 0b101;
+  PCICR |= _BV(PCIE2);
+  PCMSK2 |= _BV(PCINT18) | _BV(PCINT19) | _BV(PCINT20) | _BV(PCINT21);
   
-  // set timer compare register
-  OCR1A = RESET_DELAY;
-  TIMSK |= 1 << OCIE1A;
-
   // activate interupts
   sei();
 }
 
 void loop() {
+  idle();
+  //blink();
+}
+
+void enable_reset_timer() {
+  cli();
+  
+  // reset
+  TCCR1A = 0;
+  TCCR1B = 0;
+
+  // set up timer 1 to prescaler / 1024
+  TCCR1B |= _BV(CS12) | _BV(CS10);
+  
+  // set to CTC Mode with OCR1A as top value
+  TCCR1B |=  _BV(WGM12);
+  TCNT1 = 0;
+  // activate timer counter compare register
+  TIMSK1 = _BV(OCIE1A);
+  // set timer compare register
+  OCR1A = RESET_DELAY;  
+  
+  sei();
+}
+
+void disable_reset_timer() {
+  // activate timer counter compare register
+  TIMSK1 = 0;
+}
+
+
+// power modes
+void idle() {
+  // in idle the main loop is paused, but the timer and the pcint are running
+  SMCR = _BV(SE); // set to idle
   asm("sleep");
-  //PORTB = LAMP_MASK & (TCNT1>>4);
+}
+
+#define PRTWI0 7
+#define PRSPI0 2
+void powerdown() {
+  // in powerdown the timers and main loop are stopped, can only be woken up by PCINT (turning a switch)
+  // set sleep mode to powerdown
+  SMCR = _BV(SM1) | _BV(SE);
+  // disable everthing that costs power
+  //PRR = _BV(PRTWI0) | _BV(PRTIM2) | _BV(PRTIM0) | _BV(PRTIM1) | _BV(PRSPI0) | _BV(PRUSART0) | _BV(PRADC); 
+  asm("sleep");
+}
+
+void set_lamp(lamp_t lamp, bool state) {
+  if(lamp == 0) digitalWrite(LAMP1, state);
+  if(lamp == 1) digitalWrite(LAMP2, state);
+  if(lamp == 2) digitalWrite(LAMP3, state);
+  if(lamp == 3) digitalWrite(LAMP4, state);
+}
+
+bool get_switch(switch_t swit) {
+  if(swit == 0) return digitalRead(SWITCH1);
+  if(swit == 1) return digitalRead(SWITCH2);
+  if(swit == 2) return digitalRead(SWITCH3);
+  if(swit == 3) return digitalRead(SWITCH4);
+}
+
+uint8_t get_switches() {
+  return digitalRead(SWITCH4) << 3 | digitalRead(SWITCH3) << 2 | digitalRead(SWITCH2) << 1 | digitalRead(SWITCH1);
 }
 
 ISR(TIMER1_COMPA_vect) {
@@ -123,12 +169,13 @@ ISR(TIMER1_COMPA_vect) {
     current_lamp = 0;
     sequence = last_changed;
 
-    // deactivate timer
-    TIMSK &= ~(1 << OCIE1A);
+    disable_reset_timer();
     // got to sleep
-    powerdown();
+    //powerdown();
   }
-  TCNT1 = 0;
+
+
+  disable_reset_timer();
 }
 
 // returns a lamp without a connection. Calling this function more than LAMP_COUNT times causes an error!
@@ -139,15 +186,15 @@ byte get_next_lamp() {
 }
 
 // a switch has been switched
-ISR(PCINT_vect)
+ISR(PCINT2_vect)
 {
   // idle while active, so timers are running
-  idle();
+  //idle();
   
-  current_switches = PINB & SCHWITCH_MASK;
-  byte changed = current_switches ^ last_switches;
+  current_switches = get_switches();
+  uint8_t changed = current_switches ^ last_switches;
 
-  for(byte i = 0; i < LAMP_COUNT; i++) {
+  for(uint8_t i = 0; i < LAMP_COUNT; i++) {
     // switch i changed
     if((changed >> i) & 1) {
       // remember which switch was last changed
@@ -162,29 +209,37 @@ ISR(PCINT_vect)
   
 
   // sets the lamps by their connected switches
-  byte lamps = 0b0000;
-  for(byte i = 0; i < LAMP_COUNT; i++) {
+  for(uint8_t i = 0; i < LAMP_COUNT; i++) {
     if(connections[i] != DISCONNECTED)
-      lamps |= ((current_switches >> i) & 1) << connections[i];
+      set_lamp(connections[i], get_switch(i));
   }
-  
-  PORTB = LAMP_MASK & (lamps << LAMP_SHIFT);
+
   last_switches = current_switches;
 
   // if all switches turned off
   if(!current_switches) {
-    // reset and activate timer
-    TCNT1 = 0;
-    TIMSK |= 1 << OCIE1A;
+    enable_reset_timer();
   }
 }
 
+
 void fail() {
   while(1) {
-    PORTB = LAMP_MASK & 0xFF;
-    delay(100);
-    PORTB = LAMP_MASK & 0;
+    blink();
     delay(100);
   }
+}
+
+void blink() {
+  for(uint8_t i = 0; i < LAMP_COUNT; i++) {
+    set_lamp(i, ON);
+  }
+  //for(uint16_t i = 0; i < 16000000; i++) asm("nop");
+  delay(500);
+  for(uint8_t i = 0; i < LAMP_COUNT; i++) {
+    set_lamp(i, OFF);
+  }
+  delay(500);
+  //for(uint16_t i = 0; i < 16000000; i++) asm("nop");
 }
 
